12199d6dbcbe8f6dc4e694f1ce1861930b7c5bc9
Mayur Khadde <102829101+MK-C0dE5@users.noreply.github.com>
2023-07-11 23:02:50 +0530

    Maintain

--- DS/Sort/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-void printarray(int a[], int size){
-    for(int i=0; i<size; i++){
-        printf("%d\t",a[i]);
-    }
-    printf("\n");
-}
--- TRF/1. Quick Sort/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-if (low < j)
-    quickSort2(arr, low, j);
-    if (i < high)
-    quickSort2(arr, i, high);
